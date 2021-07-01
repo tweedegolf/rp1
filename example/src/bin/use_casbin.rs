bae64352302e832892bc9139a0617cdb0b25f3bd
Folkert <folkert@folkertdev.nl>
2021-07-01 16:19:49 +0200

    add casbin example

---
+++ example/src/bin/use_casbin.rs
@@ -0,0 +1,82 @@
+#[macro_use]
+extern crate diesel;
+
+extern crate rocket;
+
+#[path = "../schema.rs"]
+mod schema;
+
+use rocket_sync_db_pools::database;
+
+#[database("diesel")]
+struct Db(diesel::PgConnection);
+
+#[rocket_crud::crud(database = "Db", table_name = "users")]
+#[derive(serde::Serialize, diesel::Queryable)]
+struct User {
+    #[primary_key]
+    id: i32,
+    username: String,
+    #[generated]
+    created_at: chrono::NaiveDateTime,
+    #[generated]
+    updated_at: chrono::NaiveDateTime,
+}
+
+#[rocket_crud::crud(database = "Db", table_name = "posts")]
+#[derive(serde::Serialize, diesel::Queryable)]
+struct Post {
+    #[primary_key]
+    id: i32,
+    title: String,
+    subtitle: Option<String>,
+    content: String,
+    user_id: i32,
+    #[generated]
+    created_at: chrono::NaiveDateTime,
+    #[generated]
+    updated_at: chrono::NaiveDateTime,
+}
+
+#[rocket_crud::crud(database = "Db", table_name = "comments")]
+#[derive(serde::Serialize, diesel::Queryable)]
+struct Comment {
+    #[primary_key]
+    id: i32,
+    content: String,
+    #[serde(default)]
+    approved: bool,
+    post_id: i32,
+    #[not_sortable]
+    user_id: Option<i32>,
+    anonymous_user: Option<String>,
+    #[generated]
+    created_at: chrono::NaiveDateTime,
+    #[generated]
+    updated_at: chrono::NaiveDateTime,
+}
+
+#[rocket::launch]
+async fn rocket() -> _ {
+    use casbin::{DefaultModel, FileAdapter};
+
+    let m = match DefaultModel::from_file("src/bin/rbac_with_pattern_model.conf").await {
+        Ok(m) => m,
+        Err(e) => panic!("{:?}", e),
+    };
+
+    let a = FileAdapter::new("src/bin/rbac_with_pattern_model.csv");
+
+    let casbin_fairing = match rocket_crud::access_control::PermissionsFairing::new(m, a).await {
+        Ok(f) => f,
+        Err(e) => panic!("{:?}", e),
+    };
+
+    rocket::build()
+        .attach(casbin_fairing)
+        .attach(rocket_crud::access_control::AlwaysAdminFairing)
+        .mount("/users", User::get_routes())
+        .mount("/posts", Post::get_routes())
+        .mount("/comments", Comment::get_routes())
+        .attach(Db::fairing())
+}
